11e6164ecfd92e97b85d4dc4eec487d0c54fecb2
JamesQFreeman <ws.bond1@gmail.com>
2018-06-29 16:49:59 +0800

    完美曲线更新
    1. 设计了全新的显示函数，能非常优雅的显示血压。
    2. 对于openM1_closeM2()这种面向底层的函数做了封装和抽象，抽象成为充气和放气函数
    3. 改进了一些缩进问题

--- main/main.ino
+++ main/main.ino
@@ -6,8 +6,9 @@
 /*
  * 定义了ADC
  * 走I2C
+ * 16bit
  */ 
-Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+Adafruit_ADS1115 ads;
 
 /*
  * 定义ADC的输入口
@@ -26,6 +27,7 @@ int drive_input2 = 2;
 int drive_input3 = 15;
 int drive_input4 = 13;
 
+
 /*
  * 输入压力和波形值
  * serial输出“压力，波形\n”
@@ -38,14 +40,57 @@ void serial_display_pressure_and_wave(float pres, int wave){
 }
 
 /*
+ * 这个函数用来处理波形信号的神秘问题，并且进行缩放，固定在100-150左右
+ * 问题：前几秒会变得很大有溢出
+ * 把前几秒去掉，前_MAGIC_k个值去掉。
+ * 缩小_MAGIC_NUM倍
+ */
+int pure_magic(int wave, int order){
+    int _MAGIC_NUM = 50;
+    int temp = 60;
+    temp = wave/_MAGIC_NUM;
+    if (temp>200){
+      return 60;
+    }
+    else{
+      return temp;
+    }
+}
+
+/*
+ * 充气函数
+ * 充气保持time毫秒
+ */
+void inflate_for_x_ms(int time){
+    openM1_closeM2();
+    delay(time);
+}
+
+/*
+ * 缓慢放气函数
+ */
+void deflate(){
+    closeM1_closeM2();
+}
+
+/*
+ * 快速放气函数
+ * 快速放气保持time毫秒
+ */
+void quick_deflate_for_x_ms(int time){
+    closeM1_openM2();
+    delay(time);
+}
+
+/*
  * 打开OUT1和OUT2之间的电机M1，在此项目中为充气电机
  * 关闭OUT3和OUT4之间的电机M2，在此项目中为放气气阀
  */ 
 void openM1_closeM2(){
-  digitalWrite(drive_input1,HIGH);
-  digitalWrite(drive_input2,LOW);
-  digitalWrite(drive_input3,LOW);
-  digitalWrite(drive_input4,LOW);
+    digitalWrite(drive_input1,HIGH);
+    digitalWrite(drive_input2,LOW);
+    digitalWrite(drive_input3,LOW);
+    digitalWrite(drive_input4,LOW);
 }
 
 /*
@@ -53,10 +98,10 @@ void openM1_closeM2(){
  * 关闭OUT3和OUT4之间的电机M2，在此项目中为放气气阀
  */ 
 void closeM1_closeM2(){
-  digitalWrite(drive_input1,LOW);
-  digitalWrite(drive_input2,LOW);
-  digitalWrite(drive_input3,LOW);
-  digitalWrite(drive_input4,LOW);
+    digitalWrite(drive_input1,LOW);
+    digitalWrite(drive_input2,LOW);
+    digitalWrite(drive_input3,LOW);
+    digitalWrite(drive_input4,LOW);
 }
 
 /*
@@ -64,10 +109,10 @@ void closeM1_closeM2(){
  * 打开OUT3和OUT4之间的电机M2，在此项目中为放气气阀
  */ 
 void closeM1_openM2(){
-  digitalWrite(drive_input1,LOW);
-  digitalWrite(drive_input2,LOW);
-  digitalWrite(drive_input3,HIGH);
-  digitalWrite(drive_input4,LOW);
+    digitalWrite(drive_input1,LOW);
+    digitalWrite(drive_input2,LOW);
+    digitalWrite(drive_input3,HIGH);
+    digitalWrite(drive_input4,LOW);
 }
 
 /*
@@ -76,8 +121,8 @@ void closeM1_openM2(){
  * output: float
  */ 
 float transfer_adc2v(int n){
-  float t = (n * 0.1875)/1000;
-  return t;
+    float t = (n * 0.1875)/1000;
+    return t;
 }
 
 /*
@@ -86,11 +131,11 @@ float transfer_adc2v(int n){
  * output: float
  */ 
 float transfer_v2p(float v){
-  float v1 = v/5.0;
-  float v2 = v1-0.04;
-  float kpa = v2/0.018;
-  float mmhg = 7.5*kpa;
-  return mmhg;
+    float v1 = v/5.0;
+    float v2 = v1-0.04;
+    float kpa = v2/0.018;
+    float mmhg = 7.5*kpa;
+    return mmhg;
 }
 
 
@@ -133,18 +178,18 @@ void loop(void){
      * _STEP_TIME 每次充气时长 ms单位
      */ 
     const int DEFLATION_TIME = 10;
-    const int TARGET_PRESSURE = 150;
+    const int TARGET_PRESSURE = 180;
     const int _SAMPLING_RATE = 100;
     const int _STEP_TIME = 100;
     const int QUICK_DEF_TIME = 20000;
-
-    //确定要采集几个数据点
+    // 确定要采集几个数据点
     int DATA_ARRAY_SIZE = (DEFLATION_TIME*_SAMPLING_RATE);
+    // 循环里每采样后的一次delay
+    int SAMPLING_DELAY = 1000/_SAMPLING_RATE;
+
+    // 开两个数组存储数据
     int wave_data[DATA_ARRAY_SIZE];
     float pressure_data[DATA_ARRAY_SIZE];
-    int counter = DATA_ARRAY_SIZE;
-    //循环里每采样后的一次delay
-    int SAMPLING_DELAY = 1000/_SAMPLING_RATE;
 
     /*
      * 打气功能
@@ -152,8 +197,7 @@ void loop(void){
      * _STEP_TIME控制每一次打气时间，ms单位
      */
     if(pres<TARGET_PRESSURE){
-        openM1_closeM2();
-        delay(_STEP_TIME);
+        inflate_for_x_ms(_STEP_TIME);
     }
     /*
      *完成打气
@@ -161,14 +205,14 @@ void loop(void){
     else{
         closeM1_closeM2();
         //获取数据DATA_ARRAY_SIZE次
-        for( ; counter!=0; counter--){
+        for(int counter = 0; counter < DATA_ARRAY_SIZE; counter++){
             int adc_wave = ads.readADC_SingleEnded(WAVE_SIG);
             adc_pressure = ads.readADC_SingleEnded(PRES_SIG);
             pres = transfer_v2p(transfer_adc2v(adc_pressure));
-            serial_display_pressure_and_wave(pres,adc_wave);
+            //显示 pressure和wave，中间用逗号隔开，显示两条曲线
+            serial_display_pressure_and_wave(pres,pure_magic(adc_wave,counter));
             delay(SAMPLING_DELAY);
         }
-        closeM1_openM2();
-        delay(QUICK_DEF_TIME);
+        quick_deflate_for_x_ms(QUICK_DEF_TIME);
   }
 }
